96b546c1eed114ce2e385994d7bc69b196f290d7
Sharon Connell <sharon.connell@careerbuilder.com>
2015-03-04 20:08:57 -0500

    key change

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,10 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
   config.cache_classes = true
-
+  Cb.configure do |config|
+    config.dev_key    = 'WDHM2SQ6YLLT361PRNZF'
+    config.time_out   = 5
+  end
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
